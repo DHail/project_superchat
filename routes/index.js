a585d3e16ffeddb32dddbbf4d985914be0d27bb7
Jerry Gao <gao@blackwright.com>
2017-04-05 13:03:21 -0400

    start roomsAuthors function

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,16 @@
 const express = require('express');
 let router = express.Router();
 let chat = require('../lib/chatRoom')
-const redis = require('redis');
-const redisClient = redis.createClient();
 
 const index = (io) => {
 
   router.get('/', (req, res) => {
-    
-  	chat.createMessage("Axe Talk 2", "joe", "This is a message", Date.now());
 
-
-
-  	
-
-
-
-  	// redisClient.hgetall("uniqueID", (err, results) => {
-   //      console.log(results);
-   //    });
-    res.render('index' );
+  	chat.createMessage("Sword Talk 1", "jon", "This is a message", Date.now());
+    chat.roomsAuthors().then(rooms => {
+      console.log(rooms);
+      res.render('index', { rooms });
+    });
   });
 
 
