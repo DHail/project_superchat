c93a17386841c6f3fb33506f084f17eff94482ca
Jerry Gao <gao@blackwright.com>
2017-04-05 14:00:44 -0400

    finish function for displaying rooms with authors

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,23 @@ let chat = require('../lib/chatRoom')
 const index = (io) => {
 
   router.get('/', (req, res) => {
-
-  	chat.createMessage("Axe Talk 1", "jon", "This is a message", Date.now());
+  	chat.createMessage("Axe Talk 2", "jerry", "This is a message", Date.now());
     chat.roomsAuthors().then(rooms => {
+      console.log(rooms);
       res.render('index', { rooms });
     });
   });
 
+  router.post('/newRoom', (req, res) => {
+    const roomName = req.body.name;
+    const author = "jon";
+    chat.createMessage(roomName, author, `This room was created by ${author}.`, Date.now());
+    res.redirect('/');
+  });
+
+  router.post('/', (req, res) => {
+    chat.createMessage("Axe Talk 1", "jon", "This is a message", Date.now());
+  });
 
 
   return router;
