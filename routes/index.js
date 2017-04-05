bea80d0e497d4b64786cbcef582efaf323271113
Jerry Gao <gao@blackwright.com>
2017-04-05 18:41:05 -0400

    add post sockets functionality

--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,66 @@
-const express = require('express');
+const express = require("express");
 let router = express.Router();
-let chat = require('../lib/chatRoom');
+let chat = require("../lib/chatRoom");
 
-const index = (io) => {
-
-  router.get('/', (req, res) => {
-
-    
-
-  	if(!req.cookies.username) {
-  		res.redirect("/login");
-  	} else {
-  		var rooms;
-	  	chat.createMessage("Axe Talk 2", "Jack", "Blah blah blah", Date.now());
-	    chat.allRoomsAuthors().then(allRooms => {
-	    	rooms = allRooms;
-	      
-	      
-	    });
-	  }
+const index = io => {
+  router.get("/", (req, res) => {
+    if (!req.cookies.username) {
+      res.redirect("/login");
+    } else {
+      chat.allRoomsAuthors().then(rooms => {
+        res.render("index", { rooms });
+      });
+    }
   });
 
-  router.get('/login', (req, res) => {
-  	if(req.cookies.username) {
-  		res.redirect('/');
-  	} else {
-  		res.render('login');
-  	}
+  router.get("/login", (req, res) => {
+    if (req.cookies.username) {
+      res.redirect("/");
+    } else {
+      res.render("login");
+    }
   });
 
-  router.post('/newRoom', (req, res) => {
+  router.post("/newRoom", (req, res) => {
     const roomName = req.body.name;
     const author = "jon";
-    chat.createMessage(roomName, author, `This room was created by ${author}.`, Date.now());
-    res.redirect('/');
+    chat.createMessage(
+      roomName,
+      author,
+      `This room was created by ${author}.`,
+      Date.now()
+    );
+    res.redirect("/");
   });
 
-  router.post('/', (req, res) => {
-    chat.createMessage("Axe Talk 1", "jon", "This is a message", Date.now());
+  router.post("/login", (req, res) => {
+    let username = req.body.username;
+    res.cookie("username", username);
+    res.redirect("/");
   });
 
-  router.post('/login', (req, res) => {
-   let username = req.body.username;
-    res.cookie('username', username);
-    res.redirect('/');
+  io.on("connection", client => {
+
+    client.on("openRoom", roomName => {
+      chat.getRoomMessages(roomName).then(messages => {
+        client.emit("displayRoom", messages);
+      });
+    });
+
+    client.on('newRoom', data => {
+      let roomName = data.roomName;
+      let author = data.userName;
+      chat.createMessage(
+        roomName,
+        author,
+        `This room was created by ${author}.`,
+        Date.now()
+      );
+      chat.getRoomMessages(roomName).then(messages => {
+        client.emit("displayRoom", messages);
+      });
+    });
   });
-
-  io.on('connection', client => {
-
-  	client.on("openRoom", (roomName) => {
-
-  		chat.getRoomMessages(roomName).then(messages => {
-	      	client.emit("displayRoom", messages)
-	      });
-
-	  });
-
-  });
-
 
   return router;
 };
