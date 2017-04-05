2510c4b78b7ef92034874f969b0303f7efb3b3fc
DHail <dhail@ymail.com>
2017-04-05 09:33:58 -0700

    got unique rooms

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,25 @@
 const express = require('express');
 let router = express.Router();
 let chat = require('../lib/chatRoom')
+const redis = require('redis');
+const redisClient = redis.createClient();
 
 const index = (io) => {
 
   router.get('/', (req, res) => {
     
-  	chat.createRoom("Axe Talk");
+  	chat.createMessage("Axe Talk 2", "joe", "This is a message", Date.now());
 
-  	// redisClient.hset("Axe talk", )
+
+
+  	
+
+
 
   	// redisClient.hgetall("uniqueID", (err, results) => {
    //      console.log(results);
    //    });
-    res.render('index');
+    res.render('index' );
   });
 
 
