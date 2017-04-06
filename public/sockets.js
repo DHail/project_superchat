7a8b9e864230efe052a7d7502c1508c3f2c15e6b
Jerry Gao <gao@blackwright.com>
2017-04-06 15:34:28 -0400

    add create room styling

--- public/sockets.js
+++ public/sockets.js
@@ -1,67 +1,66 @@
+const socket = io.connect('http://localhost:3000');
 
-  const socket = io.connect('http://localhost:3000');
+$(document).ready( () => {
 
-  $(document).ready( () => {
+  $('#create-room').click( () => {
+    $('#new-room').show();
+  })
 
-    $('#create-room').click( () => {
-      $('#new-room-form').show();
-    })
-
-    $('#room-list').on('click', '.room', event => {
-    	console.log($(event.target));
-    	const roomName = $(event.target).attr("room-name");
-    	$('#current-room').attr("room-name", roomName);
-    	socket.emit('click room', roomName);
-    });
-
-    $('#new-room-form input').keydown(event => {
-      if (event.keyCode == 13) {
-        event.preventDefault();
-        const roomName = $('input[name=name]').val();
-        const userName = $.cookie("username");
-        socket.emit('submit new room', roomName, userName);
-        $('input[name=name]').val("");
-      }
-    });
-
-    $('#new-message-form input').keydown(event => {
-      if (event.keyCode == 13) {
-        event.preventDefault();
-        let roomName = $('#current-room').attr("room-name");
-        let author = $.cookie("username");
-        let text = $('input[name=text]').val();
-        socket.emit('new message', roomName, author, text);
-        $('input[name=text]').val("");
-      }
-    });
-
-    socket.on('open room', messages => {
-      $("#current-room").children().remove();
-      messages.forEach(msg => _appendMessageToRoom(msg.author, msg.text));
-      $('#new-room').hide();
-      $('#new-message').show();
-    });
-
-    socket.on('created message', (room, author, message) => {
-      if ($('#current-room').attr('room-name') === room) {
-        _appendMessageToRoom(author, message);
-      }
-    });
-
-    socket.on('add new room', roomName => {
-      var $newListItem = $('<li>').addClass('room').attr('room-name', roomName);
-      var $navRoomName = $('<div>').addClass('room-name').attr('room-name', roomName);
-      var $navMember = $('<div>').addClass('members').attr('room-name',roomName);
-      $navRoomName.text(roomName);
-      $navMember.text('1 member');
-      $newListItem.append($navRoomName).append($navMember);
-      $('#room-list').prepend($newListItem);
-    });
+  $('#room-list').on('click', '.room', event => {
+  	console.log($(event.target));
+  	const roomName = $(event.target).attr("room-name");
+  	$('#current-room-messages').attr("room-name", roomName);
+  	socket.emit('click room', roomName);
   });
 
-  function _appendMessageToRoom(author, message) {
-    let outerDiv = $('<div>').addClass('message');
-    outerDiv.append($('<div>').addClass('author').text(author));
-    outerDiv.append($('<div>').addClass('msg-text').text(message));
-    $('#current-room').append(outerDiv);
-  }
+  $('#new-room-form input').keydown(event => {
+    if (event.keyCode == 13) {
+      event.preventDefault();
+      const roomName = $('input[name=name]').val();
+      const userName = $.cookie("username");
+      socket.emit('submit new room', roomName, userName);
+      $('input[name=name]').val("");
+    }
+  });
+
+  $('#new-message-form input').keydown(event => {
+    if (event.keyCode == 13) {
+      event.preventDefault();
+      let roomName = $('#current-room-messages').attr("room-name");
+      let author = $.cookie("username");
+      let text = $('input[name=text]').val();
+      socket.emit('new message', roomName, author, text);
+      $('input[name=text]').val("");
+    }
+  });
+
+  socket.on('open room', messages => {
+    $("#current-room-messages").children().remove();
+    messages.forEach(msg => _appendMessageToRoom(msg.author, msg.text));
+    $('#new-room').hide();
+    $('#new-message').show();
+  });
+
+  socket.on('created message', (room, author, message) => {
+    if ($('#current-room-messages').attr('room-name') === room) {
+      _appendMessageToRoom(author, message);
+    }
+  });
+
+  socket.on('add new room', roomName => {
+    var $newListItem = $('<li>').addClass('room').attr('room-name', roomName);
+    var $navRoomName = $('<div>').addClass('room-name').attr('room-name', roomName);
+    var $navMember = $('<div>').addClass('members').attr('room-name',roomName);
+    $navRoomName.text(roomName);
+    $navMember.text('1 member');
+    $newListItem.append($navRoomName).append($navMember);
+    $('#room-list').prepend($newListItem);
+  });
+});
+
+function _appendMessageToRoom(author, message) {
+  let outerDiv = $('<div>').addClass('message');
+  outerDiv.append($('<div>').addClass('author').text(author));
+  outerDiv.append($('<div>').addClass('msg-text').text(message));
+  $('#current-room-messages').append(outerDiv);
+}
